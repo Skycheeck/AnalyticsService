ef738fd7815e1e96c9ec68aa29ee94fd72940f9c
Skycheeck <Sergeyxow@yandex.ru>
2024-09-25 19:42:46 +0300

    AnalyticsServiceBehaviour

---
+++ Assets/Scripts/AnalyticsServiceBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnalyticsServiceBehaviour : MonoBehaviour
+{
+    [SerializeField] private Button _addEventButton;
+    [SerializeField] private Button _saveButton;
+        
+    private SaveManager _saveManager;
+    private AnalyticsService _analyticsService;
+
+    private void Awake()
+    {
+        _saveManager = new SaveManager();
+        _analyticsService = new AnalyticsService(new MockAnalyticsEventsSender(), _saveManager.LoadState());
+        _addEventButton.onClick.AddListener(() => _analyticsService.TrackEvent("test", Time.time.ToString(CultureInfo.InvariantCulture)));
+        _saveButton.onClick.AddListener(() =>
+        {
+            _analyticsService.SerializeEvents();
+            _saveManager.SaveState(_analyticsService.State);
+        });
+    }
+
+    private void Update()
+    {
+        _analyticsService.Tick(Time.deltaTime);
+    }
+}
